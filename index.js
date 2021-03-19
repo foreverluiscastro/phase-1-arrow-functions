b40cc79748fc3560b048a2b8ee6ad54374bd0555
luis <foreverluiscastro@gmail.com>
2021-03-18 23:55:16 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,7 @@
+const divide = function(num1, num2) {
+    return 2000 / 100;
+}
+
+const square = (parameter1) => parameter1 ** parameter1;
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
